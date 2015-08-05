89eb49b04bf27d54d439d99275811a1321b9b516
Laszlo Bekessy <borzos6@gmail.com>
2015-08-05 19:23:58 +0200

    Configuration detection from start php moved to the config repos (environment.php)

---
+++ local/bootstrap/environment.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * getEnvironment
+ * --------------------------------------------------
+ * Returns the name of the current environment.
+ * @return (string) ($envName) The name of the environment
+ * --------------------------------------------------
+ */
+function getEnvironment() {
+    return 'local';
+}
